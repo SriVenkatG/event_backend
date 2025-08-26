18330215b5ebf18bd1d031a8434862b72f07645a
SriVenkatG <2300031666@kluniversity.in>
2025-08-26 21:39:55 +0530

    Update EventManagementSystemApplication.java

--- src/main/java/com/event_management/event_management_system_backend/EventManagementSystemApplication.java
+++ src/main/java/com/event_management/event_management_system_backend/EventManagementSystemApplication.java
@@ -1,13 +1,12 @@
-package com.event_management.event_management_system_backend;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
 @SpringBootApplication
-public class EventManagementSystemApplication {
+public class EventManagementSystemApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
-		SpringApplication.run(EventManagementSystemApplication.class, args);
-	}
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EventManagementSystemApplication.class);
+    }
 
+    public static void main(String[] args) {
+        SpringApplication.run(EventManagementSystemApplication.class, args);
+    }
 }
